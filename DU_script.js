a4925b794e05f314fcc378df12f1748ac4de89d1
Pbotto_209 <pbotto209@gmail.com>
2025-10-23 03:11:04 +0000

    created separate files

---
+++ DU_script.js
@@ -0,0 +1,14 @@
+// I don't know what these rules mean, the AI came up with them??
+console.log("Hello, World!");
+
+// Or with "use strict" for strict mode
+"use strict";
+
+// Or with a comment describing the file
+/**
+ * Description of what this script does
+ * @author Your Name
+ * @date Today's date
+ */
+
+// Your code here
